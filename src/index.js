708837ae76bea0492e6089881864fd664557ee6f
ogharka <mogharka@gmail.com>
2026-06-04 21:33:25 +0545

    Suppress ENS warning

--- src/index.js
+++ src/index.js
@@ -3,5 +3,18 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 
+// Suppress ENS warning on custom testnets
+const originalWarn = console.warn;
+console.warn = (...args) => {
+  if (args[0]?.includes?.('ENS') || args[0]?.includes?.('UNSUPPORTED_OPERATION')) return;
+  originalWarn(...args);
+};
+
+const originalError = console.error;
+console.error = (...args) => {
+  if (JSON.stringify(args)?.includes?.('getEnsAddress')) return;
+  originalError(...args);
+};
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(<React.StrictMode><App /></React.StrictMode>);
